fb2978c3b45f5325fd491a2779f4aeeb5a0102a6
unknown <vagison@gmail.com>
2022-03-25 23:26:51 +0400

    Count the Zeroes solved

---
+++ practice/count_the_zeroes.py
@@ -0,0 +1,40 @@
+def countZeroes(self, arr, n):
+    start = 0
+    end = n - 1
+    mid = (start + end) // 2
+
+    def transitionPointFinder(start, end, mid):
+        if end - start == 0:
+            if arr[end] == 0:
+                return end
+
+            else:
+                return -1
+
+        elif end - start == 1:
+            if arr[start] == 0:
+                return start
+
+            elif arr[end] == 0:
+                return end
+
+            else:
+                return -1
+
+        elif arr[mid] == 1:
+            start = mid
+            mid = (start + end) // 2
+
+        elif arr[mid] == 0:
+            end = mid
+            mid = (start + end) // 2
+
+        return transitionPointFinder(start, end, mid)
+
+    transitionPoint = transitionPointFinder(start, end, mid)
+
+    if transitionPoint == -1:
+        return 0
+
+    else:
+        return n - transitionPoint
