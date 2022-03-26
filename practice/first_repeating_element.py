655289ab6af802772b62a5de02fc3ea214cbc489
unknown <vagison@gmail.com>
2022-03-26 11:19:04 +0400

    First Repeating Element solved

---
+++ practice/first_repeating_element.py
@@ -0,0 +1,20 @@
+# First Repeating Element
+# https://practice.geeksforgeeks.org/problems/first-repeating-element4018/1
+
+def firstRepeated(self, arr, n):
+    hashMap = {}
+
+    for item in arr:
+        if not (item in hashMap):
+            hashMap[item] = 1
+        else:
+            hashMap[item] += 1
+
+    searchedElement = -1
+
+    for index in range(0, n):
+        if hashMap[arr[index]] > 1:
+            searchedElement = index + 1
+            break
+
+    return searchedElement
